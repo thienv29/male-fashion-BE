1f9077880aa4a68ae1a88b82c5b0249bb43f0322
thienv29 <thienv29@gmail.com>
2022-10-06 18:32:13 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -33,10 +33,6 @@ const startServer = () => {
         },
         credentials: true,
     }));
-    // app.use(cors({
-    //     origin: "http://localhost:3200",
-    //     credentials: true,
-    // }))
     app.use(cookiePaser());
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
